8060f448c1de24d4bcbc632c6400fe3b0add391a
tamura-tatuya <tatuya661199@gmail.com>
2019-07-04 20:57:00 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 
   def index
-    @users =  User.where('name LIKE(?) AND id != ?', "%#{params[:keyword]}%", current_user.id).limit(10)
-    respond_to do |format|
-    format.html
-    format.json
+    @users =  User.where('name LIKE(?) AND id != ?', "%#{params[:keyword]}%", current_user.id).
+      respond_to do |format|
+        format.html
+        format.json
     end 
   end
 
