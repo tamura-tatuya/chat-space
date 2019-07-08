5f83bfe5aef63ce4b7558fa3fbcd730bee846c08
tamura-tatuya <tatuya661199@gmail.com>
2019-07-08 18:57:15 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,8 +2,8 @@ $(document).on('turbolinks:load',function(){
   function buildHTML(message){
     image = ( message.image_url ) ? `<img class= "lower-message__image" src=${message.image_url} >` : "";
     var html = 
-    `<div class="message">
-        <div class="upper-message" data-message-id=${message.id}>
+    `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
           <div class="upper-message__user-name">
           ${message.user_name}
           </div>
@@ -20,28 +20,68 @@ $(document).on('turbolinks:load',function(){
           </div>`
     return html;
   }
- $('#new_message').on('submit',function(e){
-   e.preventDefault();
-     var formData = new FormData(this);
-     var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-  })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html)
-    $('form')[0].reset();
-    $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight}, 'fast'); 
-    // .message_contentでメッセージを書き込むクラスを指定、Val('')で実行するたびに記述するとこを消す
-  })
-  .fail(function(){
-    alert('error');
+
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+     $.ajax({
+       url: url,
+       type: 'POST',
+       data: formData,
+       dataType: 'json',
+       processData: false,
+       contentType: false
+   })
+   .done(function(data){  
+     var html = buildHTML(data);
+     $('.messages').append(html)
+     $('form')[0].reset();
+     $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight}, 'fast'); 
+     // .message_contentでメッセージを書き込むクラスを指定、Val('')で実行するたびに記述するとこを消す
+   })
+   .fail(function(){
+     alert('error');
+   });
+   return false;
   });
-  return false;
- });
+
+  var reloadMessages = function() {  
+    
+    group_id = $('.main-header__left-box__current-group').data("group-id");
+
+    if ($('div').hasClass('form')){
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $('.message:last').data("message-id"); //dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
+      // ここの記述でメッセージテーブルのidの一番最後のものを持ってくる。
+      $.ajax({
+        //ルーティングで設定した通りのURLを指定
+        url: `/groups/${group_id}/api/messages/`, //サーバを指定。今回はgroups/:group_id/api/messagesに処理を飛ばす
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'GET',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {group_id: group_id,
+        last_id: last_message_id}
+      })
+      .done(function(messages) {
+        // 0の時はスクロールしないようにする
+        var insertHTML = '';
+        messages.forEach(function (message){
+          insertHTML = buildHTML(message); 
+          $('.messages').append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        })
+      
+      })
+    .fail(function() {
+      });
+
+    }
+    
+  }
+  setInterval(reloadMessages, 
+    5000);
 });
+
+// メッセージ画面でのみ自動更新できるようにする
